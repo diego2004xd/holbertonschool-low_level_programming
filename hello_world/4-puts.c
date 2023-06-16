88b7e0e535ca4546ea8f574e38e3a9c1ea01e194
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-16 15:14:03 -0700

    ejer4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * five: 4-puts.c
+ * auth: diego beltran
+ */
+#include <stdio.h>
+
+/**
+ * main - prints exactly "Programming is
+ *        like building a multilingual puzzle
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	pust(programming is like building a multilingual puzzle");
+	return (0);
+}
