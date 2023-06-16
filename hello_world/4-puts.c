0514bf9cb793170cd82400e92e000d8324d6111f
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-16 15:34:47 -0700

    cd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 <<<<<<< HEAD
 	puts("/"Programming is like building a multilingual puzzle");
 =======
-	puts.c("\"programming is like building a multilingual puzzle");
+	puts.c("/"programming is like building a multilingual puzzle");
 >>>>>>> 88497082046d3e879682bb14e62be7a17949f3d6
 	return (0);
 }
