8ace50cf0d9d9702cfa9076f4598484d5d49a87a
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-21 14:43:14 -0700

    ejer2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int number = 0;
+
+	while (number <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+		number++;
+	}
+}
