4b74f03cfdc1a26564c51d10a5e3bf50fbc1fa06
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-20 19:43:51 -0500

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- *  * main - main block
- *   * Description: Positive anything is better than negative nothing
- *    * Return: Always 0 (Success)
- *     */
+ * main - main block
+ * Description: Positive anything is better than negative nothing
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -16,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	printf("%i is positive\n", n);
+		printf("%i is positive\n", n);
 	else if (n == 0)
-	printf("%i is zero\n", n);
+		printf("%i is zero\n", n);
 	else
-	printf("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	return (0);
 }
