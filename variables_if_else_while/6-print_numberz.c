5f6624c77708cc7ebe526f03dc6c25e775cac138
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-21 07:05:19 -0700

    ejr6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: This program print the lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int abc;
+		for (abc = 48; abc <= 57; abc++)
+		{
+			putchar(abc);
+		}
+	putchar('\n');
+	return (0);
+}
+
