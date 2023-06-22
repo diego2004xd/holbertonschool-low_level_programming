e37679fad60877ad776eb231b63043bd0183c652
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-22 11:21:20 -0500

    Update 10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- *  * main - prints 10 times the alphabet
- *   *
- *    * Return: Always 0.
- *     */
+ * main - prints 10 times the alphabet
+ *
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
-		char letter;
-			int number = 0;
+	char letter;
+	int number = 0;
 
-				while (i <= 9)
-						{
-									for (letter = 'a'; letter <= 'z'; letter++)
-												{
-																_putchar(letter);
-																		}
+	while (i <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
 
-											_putchar('\n');
+		_putchar('\n');
 
-													number ++;
-														}
+		number ++;
+	}
 }
