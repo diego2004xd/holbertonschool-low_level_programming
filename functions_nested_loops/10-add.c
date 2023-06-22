39c3f210534a3f457a2b822901ab934aef0c85fe
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-22 09:20:33 -0700

    ejer10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  * main - prints 10 times the alphabet
+ *   *
+ *    * Return: Always 0.
+ *     */
+void print_alphabet_x10(void)
+{
+		char letter;
+			int number = 0;
+
+				while (i <= 9)
+						{
+									for (letter = 'a'; letter <= 'z'; letter++)
+												{
+																_putchar(letter);
+																		}
+
+											_putchar('\n');
+
+													number ++;
+														}
+}
