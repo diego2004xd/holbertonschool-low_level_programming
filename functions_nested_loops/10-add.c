992906de2a369155d61e0983638763c3bc4bb08f
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-22 19:12:54 -0700

    ejer10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,24 +1,12 @@
 #include "main.h"
-
 /**
- *  * main - prints 10 times the alphabet
- *   *
- *    * Return: Always 0.
- *     */
-void print_alphabet_x10(void)
-{
-		char letter;
-			int number = 0;
-
-				while (i <= 9)
-						{
-									for (letter = 'a'; letter <= 'z'; letter++)
-												{
-																_putchar(letter);
-																		}
-
-											_putchar('\n');
-
-													number ++;
-														}
-}
+ * add - that adds two integers and returns the result
+ * @numOne: first number
+ * @numTwo: second number
+ *
+ * Return: always 0.
+ *\
+ int add(int numOne, int numTwo)
+ { 
+    return (numOne + numTwo);
+ }
