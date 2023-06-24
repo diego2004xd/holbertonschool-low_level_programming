eff9d93d43f5867d86277f2221a78c6c7a3a017c
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-24 07:22:00 -0700

    ejer6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+/*
+ *  * File: 6-print_line.c
+ *   * Auth: diego
+ *    */
+
+#include "main.h"
+
+/**
+ *  * print_line - draws a straight line in the terminal
+ *   * @n: number of times the character
+ *    *
+ *     * Return: Always 0.
+ *      */
+void print_line(int n)
+{
+		int lines;
+
+			if (n > 0)
+					{
+								for (lines = 0; lines < n; lines++)
+												_putchar('_');
+									}
+
+				_putchar('\n');
+}
