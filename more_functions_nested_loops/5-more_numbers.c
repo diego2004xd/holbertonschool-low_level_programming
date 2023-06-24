89179e4b73f618bbcb569c8a16aa52c8c61f0c06
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-24 09:20:28 -0500

    Update 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,27 @@
 /*
- *  * File: 5-more_numbers.c
- *   * Auth: diego/
+ * File: 5-more_numbers.c
+ * Auth: Diego beltran
+ */
 
 #include "main.h"
 
 /**
- *  * more_numbers - prints 10 times the numbers
- *   *
- *    * Return: Always 0.
- *     */
+ * more_numbers - prints 10 times the numbers
+ *
+ * Return: Always 0.
+ */
 void more_numbers(void)
 {
-		int num, count;
+	int num, count;
 
-			for (count = 0; count <= 9; count++)
-					{
-								for (num = 0; num <= 14; num++)
-											{
-															if (num > 9)
-																				_putchar((num / 10) + '0');
-																		_putchar((num % 10) + '0');
-																				}
-										_putchar('\n');
-											}
+	for (count = 0; count <= 9; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
