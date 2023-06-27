45cda9968b93dd6197163c6ecb1df89e640a4227
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-27 09:58:25 -0500

    Update 1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,19 +1,19 @@
 /*
- *  * File: 1-swap.c
- *   * Auth: David Vasquez
- *    */
+ * File: 1-swap.c
+ * Auth: David Vasquez
+ */
 
 #include "main.h"
 
 /**
- *  * swap_int - swaps the values of two integer
- *   * @a: first pointer
- *    * @b: second pointer
- *     */
+ * swap_int - swaps the values of two integer
+ * @a: first pointer
+ * @b: second pointer
+ */
 void swap_int(int *a, int *b)
 {
-		int firstValue = *a;
+	int firstValue = *a;
 
-			*a = *b;
-				*b = firstValue;
+	*a = *b;
+	*b = firstValue;
 }
