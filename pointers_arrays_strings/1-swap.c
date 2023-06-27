e78789055e746786bf6cbf2e5f0b7319d5f6d7ba
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-27 07:57:32 -0700

    ejer1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+/*
+ *  * File: 1-swap.c
+ *   * Auth: David Vasquez
+ *    */
+
+#include "main.h"
+
+/**
+ *  * swap_int - swaps the values of two integer
+ *   * @a: first pointer
+ *    * @b: second pointer
+ *     */
+void swap_int(int *a, int *b)
+{
+		int firstValue = *a;
+
+			*a = *b;
+				*b = firstValue;
+}
