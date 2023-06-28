cd2077b9cd42aa1c6bfb45bee2c2f73861bc505a
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-28 07:21:40 -0700

    ejer3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    char *str;
+void _puts(char *str)
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
 }
