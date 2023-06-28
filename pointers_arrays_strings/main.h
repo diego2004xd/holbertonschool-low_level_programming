e4ddec1dd495d592de5c5d7c1cfeb7fabc4671d9
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-28 07:03:36 -0700

    a

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,9 +12,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-<<<<<<< HEAD
 void _puts(char *);
-=======
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -25,4 +23,3 @@ int _atoi(char *s);
 
 
 #endif
->>>>>>> 3816e8ffcf9061b0c8b21cbe5ad54d3fda33f562
