7461f1fdb6f8acaa7a54e5e3fa2572692f705bb1
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-29 10:12:32 -0500

    Update 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- *  * _strncat - concatenates two strings
- *   * @dest: destination
- *    * @src: font
- *     * @n: max bytes
- *      *
- *       * Return: Final string concatenated
- *        */
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: font
+ * @n: max bytes
+ *
+ * Return: Final string concatenated
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-		char *stringFinal = dest;
+	char *stringFinal = dest;
 
-			while (*dest != '\0')
-						dest++;
+	while (*dest != '\0')
+		dest++;
 
-				while (n > 0 && *src != '\0')
-						{
-									*dest = *src;
-											dest++;
-													src++;
-															n--;
-																}
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
 
-					*dest = '\0';
+	*dest = '\0';
 
-						return (stringFinal);
+	return (stringFinal);
 }
