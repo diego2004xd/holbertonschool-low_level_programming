dbc6619794890c2c60ee571c8eba5289305c91fb
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-29 10:14:41 -0500

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- *  * _strncpy - copies a string
- *   * @dest: chain of destinity
- *    * @src: font of origin
- *     * @n: cantities a copy
- *      *
- *       * Return: cantities of values
- *        */
+ * _strncpy - copies a string
+ * @dest: chain of destinity
+ * @src: font of origin
+ * @n: cantities a copy
+ *
+ * Return: cantities of values
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-		int index;
+	int index;
 
-			for (index = 0; index < n && src[index] != '\0'; index++)
-						dest[index] = src[index];
+	for (index = 0; index < n && src[index] != '\0'; index++)
+		dest[index] = src[index];
 
-				for (; index < n; index++)
-							dest[index] = '\0';
+	for (; index < n; index++)
+		dest[index] = '\0';
 
-					return (dest);
+	return (dest);
 }
