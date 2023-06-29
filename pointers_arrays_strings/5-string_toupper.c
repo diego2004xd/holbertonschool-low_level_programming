6130d744451fa6c1e3325f1d50be95562c565d26
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-29 10:22:00 -0500

    Update 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- *  * string_toupper - changes all lowercase letters of a string to uppercase
- *   * @letter: patameter pointer
- *    *
- *     * Return: uppercase
- *      */
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @letter: patameter pointer
+ *
+ * Return: uppercase
+ */
 
 char *string_toupper(char *letter)
 {
-		char *pointer = letter;
+	char *pointer = letter;
 
-			while (*pointer != '\0')
-					{
-								if (*pointer >= 'a' && *pointer <= 'z')
-											{
-															*pointer = *pointer - 32;
-																	}
-										pointer++;
-											}
+	while (*pointer != '\0')
+	{
+		if (*pointer >= 'a' && *pointer <= 'z')
+		{
+			*pointer = *pointer - 32;
+		}
+		pointer++;
+	}
 
-				return (letter);
+	return (letter);
 }
