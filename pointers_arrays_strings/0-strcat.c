3b652f7e516e779cfd32459c39cef75fb029b9cd
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-29 08:08:57 -0700

    ejer0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  * _strcat - concatenates two strings
+ *   * @dest: This is the output dest
+ *    * @src: This is the input source
+ *     *
+ *      * Return: String concatenated
+ *       */
+char *_strcat(char *dest, char *src)
+{
+	char *stringFinal = dest;
+
+	while (*dest != '\0')
+        {
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (stringFinal);
+}
