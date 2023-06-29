151fe22b8af72ed5138c0dfe23ea31044c96dc36
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-29 08:14:04 -0700

    ejer2c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * _strncpy - copies a string
+ *   * @dest: chain of destinity
+ *    * @src: font of origin
+ *     * @n: cantities a copy
+ *      *
+ *       * Return: cantities of values
+ *        */
+char *_strncpy(char *dest, char *src, int n)
+{
+		int index;
+
+			for (index = 0; index < n && src[index] != '\0'; index++)
+						dest[index] = src[index];
+
+				for (; index < n; index++)
+							dest[index] = '\0';
+
+					return (dest);
+}
