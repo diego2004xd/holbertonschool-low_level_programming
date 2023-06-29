c9889e633474b705bd0cfb79946e67dafb058d77
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-29 10:16:42 -0500

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *  * _strcmp - compares two strings
- *   * @s1: first chain text
- *    * @s2: second chain text
- *     *
- *      * Return: Always 0.
- *       */
+ * _strcmp - compares two strings
+ * @s1: first chain text
+ * @s2: second chain text
+ *
+ * Return: Always 0.
+ */
 int _strcmp(char *s1, char *s2)
 {
-		while (*s1 != '\0' && *s1 == *s2)
-				{
-							s1++;
-									s2++;
-										}
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-			return (*s1 - *s2);
+	return (*s1 - *s2);
 }
