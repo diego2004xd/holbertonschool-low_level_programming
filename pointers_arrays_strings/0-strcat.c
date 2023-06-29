3c5283905b56e086b1f84ef84ff891ef3d9d9901
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-06-29 10:10:26 -0500

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *  * _strcat - concatenates two strings
- *   * @dest: This is the output dest
- *    * @src: This is the input source
- *     *
- *      * Return: String concatenated
- *       */
+ * _strcat - concatenates two strings
+ * @dest: This is the output dest
+ * @src: This is the input source
+ *
+ * Return: String concatenated
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *stringFinal = dest;
 
 	while (*dest != '\0')
-        {
+	{
 		dest++;
 	}
 
