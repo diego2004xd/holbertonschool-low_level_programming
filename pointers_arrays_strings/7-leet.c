03b8e0f697d32930eb69c6c86f00ed054765a2be
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-29 08:25:46 -0700

    ejer7c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  * leet - Write a function that encodes a string into 1337
+ *   * @letter: This is the input string
+ *    *
+ *     * Return: String converted to 1337
+ *      */
+
+char *leet(char *letter)
+{
+		int index, j;
+			char lowercase[] = {'a', 'e', 'o', 't', 'l', '\0'};
+				char uppercase[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+					char numbers[] = {'4', '3', '0', '7', '1', '\0'};
+
+						for (index = 0; letter[index] != '\0'; ++index)
+								{
+											for (j = 0; j < 5; j++)
+														{
+																		if (letter[index] == lowercase[j] || letter[index] == uppercase[j])
+																						{
+																											letter[index] = numbers[j];
+																														}
+																				}
+												}
+							return (letter);
+}
