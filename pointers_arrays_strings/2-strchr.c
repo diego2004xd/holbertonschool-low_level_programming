0c7fdf7a91ebc5bc48e181bbf98516e754155d8f
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-30 12:46:29 -0700

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,34 +1,22 @@
 #include "main.h"
 /**
- * function that locates a character in a string.
- * _strchr: function that locates a character in a string.
- * @c: chain character
- * @s: copy character
- *return: null
- */
+ *function that locates a character in a string
+ *_strchr: function that locates a character in a string.
+ *@c: letter compare
+ *@s: chain strings
+ * return: character c
+*/
 char *_strchr(char *s, char c)
 {
-<<<<<<< HEAD
-     int i;
-     
-     for (i = 0; i < c; i++);
-     {
-	     if (*s == c)
-		     return (s);
-	     if (*s == 0)
-		     return (null);
-     }
-
-     return (null);
-=======
 	for (; ; s++)
 	{
-		if (*s == c)
-			return (s);
-		if (*s == 0)
-			return (NULL);
-	}
+		if (*s==c
+		       return (s);
+		if (*s==0)
+		       return (null);
 
-	return (NULL);
->>>>>>> 843705da6e2d588be1d90fabfabc2a4dcdb28370
+
+       	}
+
+	return (null);
 }
