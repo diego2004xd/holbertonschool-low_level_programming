ce3d882ab6aca169cd99ad901fcf7a3a0d5317cc
dieg2004OXDY <camiloxd470@gmail.com>
2023-06-30 10:00:22 -0700

    gg

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,21 @@
-#include "main.h"
+[200~#include "main.h"
+
 /**
- * _strchr - locates a character in a string.
- * @s: chain strings
- * @c: letter compare
- *
- *  Return: character c
- */
+ *  * _strchr - locates a character in a string.
+ *   * @s: chain strings
+ *    * @c: letter compare
+ *     *
+ *      * Return: character c
+ *       */
 char *_strchr(char *s, char c)
 {
-	for (; ; s++)
-	{
-		if (*s == c)
-			return (s);
-		if (*s == 0)
-			return (NULL);
-	}
+		for (; ; s++)
+				{
+							if (*s == c)
+											return (s);
+									if (*s == 0)
+													return (NULL);
+										}
 
-	return (NULL);
+			return (NULL);
 }
