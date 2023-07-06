6f3279fe7f70800a0fbf88a12168277d18e3e69b
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-06 05:17:22 -0700

    ejerhjhj

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	for (; ; s++)
-	{
-		if (*s == c)
-			return (s);
-		if (*s == 0)
-			return (NULL);
-	}
+	        for (; ; s++)
+	        {
+		    if (*s == c)
+		         return ( s);
+		    if (*s == 0)
+			 return (NULL);
+		}
 
-	return (NULL);
-}
