c1eaa9893c9db53bef9bad979b51051c4c2225bf
TheMasterTz <84691244+TheMasterTz@users.noreply.github.com>
2023-07-07 22:25:05 +0000

    tack 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,30 +7,30 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int iter1, iter2;
-    unsigned int number1 = 0;
+unsigned int number1 = 0;
     unsigned int number2 = 0;
 
 	for (iter1 = 0; s[iter1] != '\0'; iter1++)
-    {
-        for (iter2 = 0; accept[iter2] != '\0'; iter2++)
-        {
-        	if (s[iter1] == accept[iter2])
-            {
-                number1++;
-                number2 = 0;
+	{
+		for (iter2 = 0; accept[iter2] != '\0'; iter2++)
+		{
+			if (s[iter1] == accept[iter2])
+			{
+				number1++;
+				number2 = 0;
 
-                break;
-            }
-            else
-            {
-                number2++;
-                if (accept[number2] == '\0')
-                {
-                    return (number1);
-                }
-            }
-        }
-    }
+				break;
+			}
+			else
+			{
+				number2++;
+				if (accept[number2] == '\0')
+				{
+					return (number1);
+				}
+			}
+		}
+	}
 
-    return (number1);
+	return (number1);
 }
