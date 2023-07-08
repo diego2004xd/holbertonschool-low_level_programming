c88a2707ff276e8ad2ea9d120ca0ea895c09a88b
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-07-08 09:01:22 -0500

    Update 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,33 +1,26 @@
-#include 'main.h'
-/* *
- *  *  function that searches a string for any of a set of bytes.
- *   * 
- *    *
- *    **/
-int _strpbrk(char *s, char *accept)
-	int iter1, iter2;
-	int number1 = 0;
-	int number2 = 0;
-	for (iter1 = 0; s[iter1] != '\0'; iter1++)
-{
-				for (iter2 = 0; accept[iter2] != '\0'; iter2++)
-				{
-				if (s[iter1] == accept[iter2])
-				{
-					number1++;
-					number2 = 0;
+#include "main.h"
 
-					break;
-			   	}
-				else
-		   		{
-					number2++;
-					if (accept[number2] == '\0')
-					{
-						return (number1);
-				   	}	
-				}
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: Init String
+ * @accept: find in String
+ *
+ * Return: byte s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index, count;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		for (count = 0; accept[count] != '\0'; count++)
+		{
+			if (accept[count] == s[index])
+			{
+				return (&s[index]);
 			}
 		}
-		return (number1);
+	}
+
+	return (NULL);
 }
