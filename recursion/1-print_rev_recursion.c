2a52b3d7d475af33df0d5709980997e7798dd52d
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-07-08 12:20:01 -0500

    Update 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - This funtion length of a string.
+ * _print_rev_recursion - This funtion print a string in reverse
  * @s : pointer to the string
- * Return: length of a string
+ *
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	s++;
-	return (_strlen_recursion(s) + 1);
+	if ((*s) != '\0')
+		{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+		}
 }
