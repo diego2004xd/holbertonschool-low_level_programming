4b2fbecec9c8189fb9177d3a2b9e6a23de0bce27
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-07-10 11:18:40 -0500

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/* *
- *  * main: parametros
- *   * @argc: inprimir
- *    * @argv: copiar
- *     */
+/**
+ * main: parametros
+ * @argc: inprimir
+ * @argv: copiar
+ */
 
 int main(int argc, char *argv[])
 {
-		printf("%s\n", argv[0]);
-			(void)argc;
-				return(0);
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return(0);
 }
-
