4ceaca37215743f545c73f2b508358a5c20810ba
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-10 12:59:21 -0700

    parametros

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - prints the number of arguments passed into it.
+ * main - prints the number of arguments passed into it
  * @argc: argument count
- * @argv: argument vector
+ * @argv: argumnet vector
  *
- * return Always 0.
+ * return: Always o.
  */
 int main(int argc, char *argv[])
 {
-	/*
-	 * al resertor 1, se obtiene el numero de argumentos de lineas de comandos
-	 * pasados al programa excluyendo el nombre del programa en si.
-	 */
-	 printf("%d\n", argc - 1);
-	 (void)argc;
-	 return (0);
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
 }
