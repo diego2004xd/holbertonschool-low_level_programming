26eb10fc818190eb8a53574166ff5eb611434e7b
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-10 09:17:59 -0700

    parametros

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* *
+ *  * main: parametros
+ *   * @argc: inprimir
+ *    * @argv: copiar
+ *     */
+
+int main(int argc, char *argv[])
+{
+		printf("%s\n", argv[0]);
+			(void)argc;
+				return(0);
+}
+
