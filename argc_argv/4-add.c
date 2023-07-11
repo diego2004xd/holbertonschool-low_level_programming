bb9a680cb5457387aa5ebac47d0fc0ea9169e4cc
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-11 05:54:40 -0700

    tack5

---
+++ argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
