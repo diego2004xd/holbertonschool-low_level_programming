54cbdbb60a2809cd0889315db1380c1776a1926f
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-10 19:02:29 -0700

    tack2

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints the number of arguments passed into it
- * main -  the number of arguments passed into it.
  * @argc: argument count
  * @argv: argumnet vector
  *
- * return: Always o.
+ * Return: Always o.
  */
 int main(int argc, char *argv[])
 {
