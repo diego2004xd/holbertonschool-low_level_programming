795a9eb0db130705d8498e42714bb915d2ee26dc
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-12 08:12:10 -0700

    tack0

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,16 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
-=======
+
 /*
  * File: main.h
  * Author: beltran
  * Description: Header file of  prototypes for functions
  */
->>>>>>> 12f3664ccc92c9fcf5648fdad1d6afccbe70071b
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
