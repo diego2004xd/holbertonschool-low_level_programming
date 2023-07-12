21ddb65ea628d643d271fe8c96faaf11d48bce72
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-12 08:05:25 -0700

    tack0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#inludes "main.h"
+#include "main.c"
 
 /**
  * function that creates an array of chars, and initializes it with a specific char.
@@ -19,7 +19,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 
 	if (s == NULL)
 			return (NULL);
-	for (iterador = 0; iterador < size; iterador++)
-			s[iterador] = c;
+	for (iterator = 0; iterator < size; iterator++)
+			s[iterator] = c;
 	return (s);
 }
