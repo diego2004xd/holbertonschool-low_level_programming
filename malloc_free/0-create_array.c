355db2f2d028672b2c1604e51499de5cc1e5f3b5
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-12 07:47:33 -0700

    tack0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,52 +1,25 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#inlude "main.h"
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * function that creates an array of chars, and initializes it with a specific char.
+ * @size: lenght of array
+ * @c: input character
  *
- * Return: Nothing.
+ * Return: pointerto the array
  */
-
-void simple_print_buffer(char *buffer, unsigned int size)
+char *create_array(unsigned int size, char c);
 {
-    unsigned int i;
+	unsigned int iterator;
+	char *s;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
+	if (size == 0)
+			return (NULL);
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *buffer;
+	s = malloc(sizeof(char) *size);
 
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
+	if (s == NULL)
+			return (NULL);
+	for (iterador = 0; iterador < size; iterador++)
+			s[iterador] = c;
+	return (s);
 }
