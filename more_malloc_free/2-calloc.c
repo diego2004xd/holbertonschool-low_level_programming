284f744c0c47d8cdea0cc64b7522deec45889108
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-07-14 15:57:21 -0400

    Update 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,1 +1,35 @@
-fhhjfqehkefhjk
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - This funtion allocates memory for an array, using malloc
+ *
+ * @nmemb : is a long of array
+ * @size : is bytes of elements
+ *
+ * Return: Pointer to Array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+
+{
+	char *ar;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ar = malloc(size * nmemb);
+
+	if (ar == NULL)
+		return (NULL);
+
+	while (i < size * nmemb)
+	{
+		ar[i] = 0;
+		i++;
+	}
+
+	return (ar);
+
+}
