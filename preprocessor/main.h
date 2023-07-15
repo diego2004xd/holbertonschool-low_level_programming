4498060a4bc887e7ad24f911eb9c5183eb434c17
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-15 08:59:23 -0700

    tack0

---
+++ preprocessor/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: diego beltran
+ * Description: Header file of  prototypes for functions
+ */
+
+int _putchar(char c);
+
+#endif
