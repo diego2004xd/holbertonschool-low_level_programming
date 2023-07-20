55114cf1774773c0b352092766e6b8c31206d1f7
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-07-20 14:33:46 -0500

    Update 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
 /**
  * print_name - This funtion prints a name
  * @name : pointer to name
@@ -8,10 +7,10 @@
  */
 
 void print_name(char *name, void (*f)(char *))
-	
+
 {
-		if (name == NULL  || f == NULL)
-				return;
-		f(name);
+	if (name == NULL  || f == NULL)
+		return;
+	f(name);
 
 }
