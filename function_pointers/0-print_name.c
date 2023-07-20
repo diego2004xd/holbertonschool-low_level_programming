e98f3010367d8af739eb85036f796d84990f9766
dieg2004OXDY <camiloxd470@gmail.com>
2023-07-20 12:31:20 -0700

    tack0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - This funtion prints a name
+ * @name : pointer to name
+ * @f : function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+	
+{
+		if (name == NULL  || f == NULL)
+				return;
+		f(name);
+
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+#include <stddef.h>
+
+/*
+ * File: main.h
+ * Author: Ricardo Valdes
+ * Description: Header file of  prototypes for functions
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
