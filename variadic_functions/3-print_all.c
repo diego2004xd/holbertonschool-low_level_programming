c5298dcb81f75dd75a0362832895a8873aec2a19
diego2004xd <135661828+diego2004xd@users.noreply.github.com>
2023-07-24 10:31:50 -0500

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,51 +3,47 @@
 #include "variadic_functions.h"
 #include <stdbool.h>
 
-/* *
- *  * print_all - Function that prints anything
- *   * @format: print value
- *    */
+/**
+ * print_all - Function that prints anything
+ * @format: print value
+ */
 
 void print_all(const char * const format, ...)
 {
-		va_list v1;
-			char *string;
-				int i;
+	va_list v1;
+	char *string;
+	int i;
 
-					i = 0;
-						va_start(v1, format);
-							while (format != NULL && format[i] != '\0')
-									{
-												switch (format[i])
-															{
-																			case 'i':
-																								printf("%i", va_arg(v1, int));
-																												break;
-																															case 'f':
-																																printf("%f", va_arg(v1, double));
-																																				break;
-																																							case 'c':
-																																								printf("%c", (char) va_arg(v1, int));
-																																												break;
-																																															case 's':
-																																																string = va_arg(v1, char *);
-																																																				if (string == NULL)
-																																																									{
-																																																															printf("(nil)");
-																																																																				break;
-																																																																								}
-																																																								printf("%s", string);
-																																																												break;
-																																																														}
-														if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
-																			format[i] == 's') && format[(i + 1)] != '\0')
-																		printf(", ");
-																i++;
-																	}
-								printf("\n");
-									va_end(v1);
+	i = 0;
+	va_start(v1, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%i", va_arg(v1, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(v1, double));
+				break;
+			case 'c':
+				printf("%c", (char) va_arg(v1, int));
+				break;
+			case 's':
+				string = va_arg(v1, char *);
+				if (string == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", string);
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(v1);
 }
-																																																									}
-															}
-									}
-}*/
